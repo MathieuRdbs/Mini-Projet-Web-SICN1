20b804477eabf23b44953a728d0d4b24f10a0a20
MathieuRdbs <mathieugilrandria@gmail.com>
2025-04-04 19:32:21 +0000

    add new design to the home page

--- resources/views/home/home_page_presentation.blade.php
+++ resources/views/home/home_page_presentation.blade.php
@@ -1,4 +1,4 @@
-<div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel" data-bs-interval="4000">
+<div id="myCarousel" class="carousel slide mb-0" data-bs-ride="carousel" data-bs-interval="4000">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1" aria-current="true"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -21,7 +21,7 @@
           <div class="carousel-caption">
             <h1>SEE OUR LATEST DEAL</h1>
             <p>We offer you all good products with less costing price. Sign up or Login and add everything to your cart.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Login</a></p>
+            <p><a class="btn btn-lg btn-primary" href="{{route('login')}}">Login</a></p>
           </div>
         </div>
       </div>
@@ -45,3 +45,64 @@
       <span class="visually-hidden">Next</span>
     </button>
 </div>
+
+<!-- OTHER Display -->
+<div class="card bg-dark text-white rounded-0">
+  <img class="card-img" src="{{asset('img/homepage/sport4.jpg')}}" alt="Card image">
+  <div class="card-img-overlay">
+    <h5 class="card-title display-5">Our Products</h5>
+    <p class="card-text">Scroll down to see few of our products. Some of them have been added recently. <br> Don't hesitate to choose and add in your cart !</p>
+
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
+
+<!-- Some products presentation -->
+<div class="container my-5">
+    <div class="box p-4 shadow-lg">
+        <h2 class="text-center mb-4">Our products</h2>
+
+        <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                @foreach($products->chunk(3) as $index => $productChunk)
+                <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                    <div class="row">
+                        @foreach($productChunk as $product)
+                        <div class="col-md-4">
+                            <div class="card text-white border-0">
+                                <img src="{{ asset('productimages/' . basename($product->image)) }}" class="card-img" alt="{{ $product->name }}">
+                                <div class="card-img-overlay d-flex flex-column justify-content-center text-center bg-dark bg-opacity-50">
+                                    <h4 class="card-title">{{ $product->name }}</h4>
+                                    <p class="card-text">{{ Str::limit($product->description, 80) }}</p>
+                                    <a href="#" class="btn btn-warning">Voir plus</a>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+                @endforeach
+            </div>
+
+            <!-- Boutons de navigation -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </button>
+        </div>
+    </div>
+</div>
+
+<style>
+    .box {
+        background: rgba(248, 249, 250, 0.8); /* Ajout de transparence */
+        border-radius: 15px;
+        border: 2px solid #dee2e6;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+    }
+</style>
+
+
+
--- resources/views/home/navbar_conn.blade.php
+++ resources/views/home/navbar_conn.blade.php
@@ -7,25 +7,38 @@
                 <img src="{{ asset('img/Sportero_transparent-.png') }}" alt="Mon Icône" width="130" height="112" class="me-2">
             </a>
     
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="{{route('homepage')}}" class="nav-link px-2 text-white">Home</a></li>
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 align-items-center">
+            <li><a href="{{route('homepage')}}" class="nav-link px-2 text-white mx-3">Home</a></li>
 
-            <li class="nav-item dropdown">
+            <li class="nav-item dropdown mx-3">
                 <a class="nav-link dropdown-toggle px-2 text-white" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Categories <span class="ms-1"><i class="bi bi-chevron-down"></i></span>
+                Products 
+                <!-- <span class="ms-1"><i class="bi bi-chevron-down"></i></span> -->
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="categoriesDropdown">
-                        <li><a class="dropdown-item" href="#">Category 1</a></li>
+                      @foreach($categories as $categorie)
+                      <li><a class="dropdown-item" href="#">{{$categorie->category_name}}</a></li>
+                      @endforeach
+
+                        <!-- <li><a class="dropdown-item" href="#">Category 1</a></li>
                         <li><a class="dropdown-item" href="#">Category 2</a></li>
-                        <li><a class="dropdown-item" href="#">Category 3</a></li>
+                        <li><a class="dropdown-item" href="#">Category 3</a></li> -->
                 </ul>
             </li>
-
-            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">Contact</a></li>
+            <li><a href="#" class="nav-link px-2 text-white mx-3">Contact</a></li>
+            @auth
+            <li class="nav-item mx-2">
+              <a class="nav-link position-relative px-2 text-white" href="#">
+              <i class="bi bi-cart" style="font-size: 1.5rem;"></i>
+              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                3 <!-- Ce chiffre sera dynamique plus tard -->
+              </span>
+            </a>
+            </li>
+            @endauth
             </ul>
-    
+
+            
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
               <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" >
             </form>
@@ -35,9 +48,7 @@
                 <img src="{{asset('img/man.png')}}" alt="mdo" width="32" height="32" class="rounded-circle">
               </a>
               <ul class="dropdown-menu text-small " data-popper-placement="bottom-end" style="position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate3d(0px, 34.2857px, 0px);">
-                <li><a class="dropdown-item" href="#">Your cart</a></li>
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><a class="dropdown-item" href="{{route('user.profile')}}">Profile</a></li>
                 <li><hr class="dropdown-divider"></li>
                 <form action="{{route('logout')}}" method="post">
                   @csrf
