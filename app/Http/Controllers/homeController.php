6a87ecba329dc8a8d6666248178a3e1f65c7abe8
MathieuRdbs <mathieugilrandria@gmail.com>
2025-04-04 19:10:28 +0000

    Adding methods to manage showing categories and products

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\Product;
+
+use App\Models\Category;
+
 class homeController extends Controller{
 
     public function showhome(Request $request){
@@ -11,4 +15,18 @@ class homeController extends Controller{
 
         return view('homepage', compact(['user']));
     }
+    
+    //pour afficher quelques produits sur la page d'accueil
+    public function index()
+    {
+        // Récupérer les 3 premiers produits (ou tous les produits que tu souhaites)
+        $products = Product::limit(6)->get();
+
+        //Retourne toutes les catégories
+        $categories = Category::all();
+
+        // Retourner la vue principale en passant les produits récupérés
+        return view('homepage', compact('categories','products'));
+    }
+
 }
